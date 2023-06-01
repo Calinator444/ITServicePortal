9f1efda93c15f55e16ba154fa44bcd31c6e1f21c
Calinator444 <65635198+Calinator444@users.noreply.github.com>
2023-06-01 11:43:01 +1000

    Fixed issue bean

--- src/main/java/classes/IssueBean.java
+++ src/main/java/classes/IssueBean.java
@@ -9,7 +9,7 @@ public class IssueBean {
         title = "";
         issueDescript = "";
         resolution = "";
-        issueID = 0;
+        issueId = 0;
     }
     private String issueStatus;
     private String reporter;
@@ -19,16 +19,22 @@ public class IssueBean {
     private String resolution;
     private Date dateTimeReport;
     private Date dateTimeResolved;
-    private int issueID;
+    private int issueId;
 
+    public List<CommentBean> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentBean> comments) {
+        this.comments = comments;
+    }
+
+    private List<CommentBean> comments;
     //getter methods
     public String getIssueStatus() {
         return issueStatus;
     }
 
-    public void setIssueStatus(String issueStatus) {
-        this.issueStatus = issueStatus;
-    }
 
     public String getTitle() {
         return title;
@@ -46,40 +52,22 @@ public class IssueBean {
     public String getFixer(){ return fixer; }
     public Date getDateTimeReport(){ return dateTimeReport; }
     public Date getDateTimeResolved(){ return dateTimeResolved; }
-    public int getIssueID(){ return issueID; }
+    public int getIssueId(){ return issueId; }
 
     //setter methods
     public void setIssueStatus(String issueStatus) {
         this.issueStatus = issueStatus;
     }
-    public void setIssueID(int issueID){ this.issueID = issueID; }
+    public void setIssueId(int issueId){ this.issueId = issueId; }
     public void setReporter(String reporter){ this.reporter = reporter; }
     public void setFixer(String fixer){this.fixer = fixer; }
-    public void setTitle(String title) {
-        this.title = title;
-    }
     public void setIssueDescript(String issueDescript) {
         this.issueDescript = issueDescript;
     }
 
-    public String getResolution() {
-        return resolution;
-    }
 
     public void setResolution(String resolution) {
         this.resolution = resolution;
     }
 
-
-    public IssueBean[] getIssues() {
-        return issues;
-    }
-    public void setIssues(IssueBean[] issues)
-    {
-        this.issues = issues;
-    }
-
-    private IssueBean[] issues;
-
-    private String issueStatus, title, issueDescript, resolution;
 }
